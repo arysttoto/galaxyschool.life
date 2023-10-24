dd922cd4ac51081b9dcbf9c224af79c7361fd154
arysttoto <aryst.toto@gmail.com>
2023-10-24 20:20:33 +0600

    likes added, and magazine added

--- app/(main)/galaxy-life/page.tsx
+++ app/(main)/galaxy-life/page.tsx
@@ -1,17 +1,60 @@
-import CountdownTimer from './countDown';
+'use client'
+import React, { useState, useEffect, useRef } from "react";
+import '@react-pdf-viewer/core/lib/styles/index.css';
+import '@react-pdf-viewer/default-layout/lib/styles/index.css';
+import { Viewer, Worker } from '@react-pdf-viewer/core';
+import { defaultLayoutPlugin } from '@react-pdf-viewer/default-layout';
 
 function GalaxyLife() {
-    return (
-        <>
-        <div className="text-center my-32 mx-5">
-        <h1 className="text-5xl md:text-6xl font-extrabold leading-tighter tracking-tighter mb-4" data-aos="zoom-y-out">We are <span className="bg-clip-text text-transparent bg-gradient-to-r from-blue-500 to-teal-400">almost</span> there!</h1>
-            <p className="text-xl text-gray-600 mb-8" data-aos="zoom-y-out" data-aos-delay="150">Galaxy Council is planned to be introduced on October 1! </p>
+  const defaultLayoutPluginInstance = defaultLayoutPlugin();
 
-            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-10 mt-10 lg:mt-20">
-                <CountdownTimer date={"2023-10-15T00:00:00"}/> 
-            </div>
+  const [selectedIssue, setSelectedIssue] = useState("issue_1"); // Default issue number
+
+  const handleIssueChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    setSelectedIssue(e.target.value);
+  };
+
+  // useEffect(() => {
+  //   // Perform any additional actions or updates when the selected issue changes.
+  //   // You can load data specific to the selected issue here.
+  // }, [selectedIssue]); 
+
+  return (
+    <>
+    <div className="mx-6 my-20 lg:my-32 lg:mx-40">
+      <label htmlFor="issue_select" className="sr-only">
+        Choose an issue
+      </label>
+      <select
+        id="issue_select"
+        className="block py-2.5 px-0 w-full text-md text-gray-500 bg-transparent border-0 border-b-2 border-gray-200 appearance-none dark:text-gray-400 dark:border-gray-700 focus:outline-none focus:ring-0 focus:border-gray-200 peer"
+        value={selectedIssue}
+        onChange={handleIssueChange}
+        style={{ color: 'black' }} // Add this inline style
+      >
+        <option value="issue_1">Latest issue</option>
+      </select>
+      <Worker workerUrl="https://unpkg.com/pdfjs-dist@3.4.120/build/pdf.worker.js">
+        <div className="lg:mt-8 mt-4 lg:m-0 m-4">
+          <div
+            style={{
+              border: '1px solid rgba(0, 0, 0, 0.3)',
+              height: '750px',
+            }}
+            className="rounded-lg bg-white dark:bg-gray-800 p-4"
+          >
+            <Viewer
+              fileUrl={`./magazines/${selectedIssue}.pdf`}
+              plugins={[
+                defaultLayoutPluginInstance,
+              ]}
+            />
+          </div>
         </div>
-        </>
-    ); 
+      </Worker>
+    </div>
+    </>
+  );
 }
-export default GalaxyLife; 
+
+export default GalaxyLife;
---
+++ app/(main)/suggestion-box/singleThought.tsx
@@ -0,0 +1,83 @@
+'use client' 
+import { Session } from "next-auth";
+import Image from "next/image";
+
+import { useState } from 'react'; // Import useState
+
+function SingleThought({ thought }: { thought: any }) {
+  const [liked, setLiked] = useState(thought.likedBySpecificUser); // Initialize liked state
+  const [likes_count, setLikes_count] = useState(thought.likes.length); 
+
+  const [isLiking, setIsLiking] = useState(false);
+
+  const toggleLike = async () => {
+    if (isLiking) {
+      return; // Prevent multiple clicks while a request is in progress
+    }
+  
+    try {
+      setIsLiking(true); // Disable the button
+    
+      setLiked(!liked);
+      setLikes_count(liked ? likes_count - 1 : likes_count + 1);
+    
+      // Send a request to your server to like/unlike the thought based on the `liked` state
+      const response = await fetch(`/api/like/thought/`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          thoughtId: thought.id,
+        }),
+      });
+    
+      if (response.ok) {
+        // Update the liked state after a successful like/unlike operation
+        const body = await response.json();
+      } else {
+        console.error('Failed to like/unlike thought');
+        setLiked(!liked); // Revert the liked state if the request fails
+      }
+    } catch (error) {
+      console.error('An error occurred while liking/unliking thought', error);
+      setLiked(!liked); // Revert the liked state on error
+    } finally {
+      setIsLiking(false); // Re-enable the button
+    }
+  };
+
+
+  return (
+    <>
+      <div className="max-w-4xl px-10 my-4 py-6 bg-white rounded-lg shadow-md">
+        <div className="flex justify-between items-center">
+          <span className="font-light text-gray-600">{String(thought.updatedAt)}</span>
+        </div>
+        <div className="mt-2">
+          <p className="mt-2 text-gray-600 text-lg font-bold">{thought.text}</p>
+        </div>
+        <div className="flex justify-between items-center mt-4">
+          <button onClick={toggleLike}>
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill={liked ? "currentColor" : "none"} stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="lucide lucide-heart text-red-600"><path d="M19 14c1.49-1.46 3-3.21 3-5.5A5.5 5.5 0 0 0 16.5 3c-1.76 0-3 .5-4.5 2-1.5-1.5-2.74-2-4.5-2A5.5 5.5 0 0 0 2 8.5c0 2.3 1.5 4.05 3 5.5l7 7Z"/></svg> 
+            <p className="text-gray-600">{likes_count}</p>
+          </button> 
+          <div>
+            <a className="flex items-center" href="#">
+              <Image
+                className="mx-4 object-cover rounded-full hidden sm:block"
+                height={25}
+                width={25}
+                src={thought.user.image || '/images/unknown.png'}
+                alt="avatar"
+              />
+              <h1 className="text-gray-700 font-bold">{thought.user.name}</h1>
+            </a>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default SingleThought;
